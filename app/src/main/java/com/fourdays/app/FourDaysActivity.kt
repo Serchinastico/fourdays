5cc069f927065b5bcc52b2426ffe17ba25d3f038
Sergio Gutiérrez <Serchinastico@users.noreply.github.com>
2019-03-22 23:23:54 +0100

    Configure base project (#4)
    * Include a database to store information with Room
    * Dependency injection with kodein
    * Navigation library
    * Testing tooling
    * Linting tools

--- app/src/main/java/com/fourdays/app/FourDaysActivity.kt
+++ app/src/main/java/com/fourdays/app/FourDaysActivity.kt
@@ -1,12 +1,11 @@
 package com.fourdays.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-
+class FourDaysActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_four_days)
     }
 }
---
+++ app/src/main/java/com/fourdays/app/setup/viewmodel/SetupViewModel.kt
@@ -0,0 +1,9 @@
+package com.fourdays.app.setup.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class SetupViewModel(
+    application: Application
+) : AndroidViewModel(application) {
+}
