ea1ea9ad888117c7c2c4fc202d2a9af24b00c1e6
Serchinastico <54cymru@gmail.com>
2019-03-22 22:40:52 +0100

    Create DI system, include jetpack navigation with its first fragment

--- app/src/main/java/com/fourdays/app/FourDaysActivity.kt
+++ app/src/main/java/com/fourdays/app/FourDaysActivity.kt
@@ -3,9 +3,9 @@ package com.fourdays.app
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class FourDaysActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_four_days)
     }
 }
