a27d8fedb6773714ef60064f0c6848888990c315
Serchinastico <54cymru@gmail.com>
2019-03-23 21:48:12 +0100

    Configure food rows and items

---
+++ app/src/main/java/com/fourdays/app/common/ui/view/FoodItemView.kt
@@ -0,0 +1,27 @@
+package com.fourdays.app.common.ui.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.fourdays.app.R
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.view_food_item.view.*
+
+class FoodItemView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr),
+    LayoutContainer {
+    override val containerView: View? = LayoutInflater.from(context)
+        .inflate(R.layout.view_food_item, this, true)
+
+    var foodName: String?
+        get() = foodItemNameTextView.text.toString()
+        set(value) {
+            foodItemNameTextView.text = value
+        }
+
+}
--- app/src/main/java/com/fourdays/app/setup/ui/SetupFragment.kt
+++ app/src/main/java/com/fourdays/app/setup/ui/SetupFragment.kt
@@ -11,6 +11,7 @@ import com.fourdays.app.R
 import com.fourdays.app.common.di.module
 import com.fourdays.app.common.ui.BaseFragment
 import com.fourdays.app.common.ui.bindViewModel
+import com.fourdays.app.common.ui.view.FoodItemView
 import com.fourdays.app.common.ui.viewModel
 import com.fourdays.app.databinding.FragmentSetupBinding
 import com.fourdays.app.setup.viewmodel.SetupViewModel
@@ -32,6 +33,16 @@ class SetupFragment : BaseFragment<FragmentSetupBinding>() {
                 dataSourceOf(
                     Description,
                     FoodGroup(getString(R.string.setup_food_group_name, "1")),
+                    FoodRow(
+                        FoodItem("Cebolla", ""),
+                        FoodItem("Lechuga", ""),
+                        FoodItem("Guisantes", "")
+                    ),
+                    FoodRow(
+                        FoodItem("Batata", ""),
+                        FoodItem("Lechuga", ""),
+                        FoodItem("Guisantes", "")
+                    ),
                     FoodGroup(getString(R.string.setup_food_group_name, "2")),
                     FoodGroup(getString(R.string.setup_food_group_name, "3")),
                     FoodGroup(getString(R.string.setup_food_group_name, "4"))
@@ -45,15 +56,31 @@ class SetupFragment : BaseFragment<FragmentSetupBinding>() {
                     groupName.text = item.name
                 }
             }
+            withItem<FoodRow>(R.layout.recycler_view_setup_food_row_item) {
+                onBind(::FoodRowViewHolder) { _, item ->
+                    foodItem1.foodName = item.firstItem.name
+                    foodItem2.foodName = item.secondItem?.name
+                    foodItem3.foodName = item.thirdItem?.name
+                }
+            }
         }
     }
 
     override fun configureBinding(binding: FragmentSetupBinding) {}
 }
 
+data class FoodItem(val name: String, val imageResourceName: String)
+
 object Description
 data class FoodGroup(val name: String)
+data class FoodRow(val firstItem: FoodItem, val secondItem: FoodItem?, val thirdItem: FoodItem?)
 class DescriptionViewHolder(itemView: View) : ViewHolder(itemView)
 class FoodGroupViewHolder(itemView: View) : ViewHolder(itemView) {
     val groupName: TextView = itemView.findViewById(R.id.groupNameTextView)
+}
+
+class FoodRowViewHolder(itemView: View) : ViewHolder(itemView) {
+    val foodItem1: FoodItemView = itemView.findViewById(R.id.foodItem1View)
+    val foodItem2: FoodItemView = itemView.findViewById(R.id.foodItem2View)
+    val foodItem3: FoodItemView = itemView.findViewById(R.id.foodItem3View)
 }
