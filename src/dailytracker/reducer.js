3245c4ea338a77f6d61380e047985e2a12131f27
Serchinastico <54cymru@gmail.com>
2019-04-16 00:39:38 +0200

    Store the selected day always in the store to have an homogeneous flow instead of using a calendar day and a non-calendar day for arrows

--- src/dailytracker/reducer.js
+++ src/dailytracker/reducer.js
@@ -1,15 +1,18 @@
-import { SELECT_DAY_IN_CALENDAR } from "./types";
+import moment from "moment";
+import { SELECT_DAY } from "./types";
 
-const initialState = {};
+const initialState = {
+	selectedDay: moment()
+};
 
-const onSelectDayInCalendar = (state, action) => {
-	return { ...state, daySelectedInCalendar: action.payload };
+const onSelectDay = (state, action) => {
+	return { ...state, selectedDay: action.payload };
 };
 
 const dailyTrackerReducer = (state = initialState, action) => {
 	switch (action.type) {
-		case SELECT_DAY_IN_CALENDAR:
-			return onSelectDayInCalendar(state, action);
+		case SELECT_DAY:
+			return onSelectDay(state, action);
 		default:
 			return state;
 	}
--- src/dailytracker/types.js
+++ src/dailytracker/types.js
@@ -1,2 +1,2 @@
 /* eslint-disable import/prefer-default-export */
-export const SELECT_DAY_IN_CALENDAR = "Select day in calendar";
+export const SELECT_DAY = "Select day";
