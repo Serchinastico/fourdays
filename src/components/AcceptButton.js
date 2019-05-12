8b4f5f45323d722a2be73d5e6b073b4ea2a263a6
Serchinastico <54cymru@gmail.com>
2019-05-12 22:57:08 +0200

    Add linear gradient to accept button

--- src/components/AcceptButton.js
+++ src/components/AcceptButton.js
@@ -1,22 +1,28 @@
 import React from "react";
 import { TouchableHighlight, StyleSheet, Text } from "react-native";
+import LinearGradient from "react-native-linear-gradient";
 import I18n from "../translations/i18n";
 import { color, style } from "./style/style";
 
 const styles = StyleSheet.create({
 	container: {
-		height: 48,
-		borderRadius: 24,
-		backgroundColor: color.darkMint,
+		height: 48
+	},
+	gradient: {
+		flex: 1,
 		justifyContent: "center",
-		marginHorizontal: 32,
-		elevation: 2
+		alignItems: "stretch",
+		elevation: 2,
+		borderRadius: 24,
+		marginHorizontal: 32
 	},
 	text: {
 		...style.largeMediumNeutral,
 		color: color.white,
 		textAlign: "center",
-		alignSelf: "center"
+		alignSelf: "center",
+		marginTop: 8,
+		flex: 1
 	}
 });
 
@@ -29,7 +35,12 @@ class AcceptButton extends React.PureComponent {
 				style={[styles.container, style]}
 				onPress={() => onPress()}
 			>
-				<Text style={styles.text}>{I18n.t("common.accept.text")}</Text>
+				<LinearGradient
+					colors={[color.seafoamGreen, color.darkMint]}
+					style={styles.gradient}
+				>
+					<Text style={styles.text}>{I18n.t("common.accept.text")}</Text>
+				</LinearGradient>
 			</TouchableHighlight>
 		);
 	}
