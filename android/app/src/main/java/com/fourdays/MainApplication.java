8a30c0df83d33f401d6bf6a7d66cc6cfed02ef65
Serchinastico <54cymru@gmail.com>
2019-05-25 20:35:07 +0200

    Be able to share the first PDF with just a header

--- android/app/src/main/java/com/fourdays/MainApplication.java
+++ android/app/src/main/java/com/fourdays/MainApplication.java
@@ -1,22 +1,24 @@
 package com.fourdays;
 
-import android.app.Application;
-
-import com.facebook.react.ReactApplication;
-import com.reactcommunity.rnlocalize.RNLocalizePackage;
-import com.BV.LinearGradient.LinearGradientPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import android.support.multidex.MultiDexApplication;
+import cl.json.ShareApplication;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.christopherdro.htmltopdf.RNHTMLtoPDFPackage;
+import com.facebook.react.ReactApplication;
+import cl.json.RNSharePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
+import com.reactcommunity.rnlocalize.RNLocalizePackage;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends MultiDexApplication implements ReactApplication,
+    ShareApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -26,8 +28,10 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
+      return Arrays.asList(
           new MainReactPackage(),
+            new RNSharePackage(),
+            new RNHTMLtoPDFPackage(),
             new RNLocalizePackage(),
             new LinearGradientPackage(),
             new RNGestureHandlerPackage(),
@@ -51,5 +55,9 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+  }
+
+  @Override public String getFileProviderAuthority() {
+    return BuildConfig.APPLICATION_ID + ".provider";
   }
 }
