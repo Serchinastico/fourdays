08abeff0ad091a136c2864ed5770b16f346a5a43
Serchinastico <54cymru@gmail.com>
2019-05-25 22:13:49 +0200

    Generate a good looking empty report, we just need to add the food in there

--- src/dailytracker/actions.js
+++ src/dailytracker/actions.js
@@ -1,5 +1,8 @@
 import AsyncStorage from "@react-native-community/async-storage";
 import moment from "moment";
+import * as R from "ramda";
+import RNHTMLtoPDF from "react-native-html-to-pdf";
+import Share from "react-native-share";
 import addItemToListIfPresentRemoveOtherwise from "../common/collections";
 import {
 	STORE_CONSUMED_FOOD_FOR_DAY_START,
@@ -7,6 +10,7 @@ import {
 	FETCH_CONSUMED_FOOD_FOR_DAY_START,
 	FETCH_CONSUMED_FOOD_FOR_DAY_FINISHED
 } from "./types";
+import I18n from "../translations/i18n";
 
 export const dayFormatForStoringConsumedFoodIds = "DD-MM-YYYY";
 
@@ -39,6 +43,68 @@ function dayBySubtractingDays(date, days) {
 	const updatedDate = moment(date);
 	updatedDate.subtract(days, "day");
 	return updatedDate;
+}
+
+export function shareMonthlyReport(day) {
+	return async () => {
+		const monthFormat = "MMM YYYY";
+		let contentsInHtml =
+			"<style>table{width:100%}td{color: #444;line-height: 22px;text-align: center;height: 40px;}tr:first-child td{font-size: 14px;font-weight: 200;}.dayNumber{font-size: 16px;font-weight: 800;}.foodName{font-size: 12px;font-weight: 400;}.foodName p{margin: 0;}</style><h1>Four Days</h1><h2>%TITLE% - %DATE%</h2><table><tr><td>%FIRST_DAY%</td><td>%SECOND_DAY%</td><td>%THIRD_DAY%</td><td>%FOURTH_DAY%</td><td>%FIFTH_DAY%</td><td>%SIXTH_DAY%</td><td>%SEVENTH_DAY%</td></tr>%CONTENT%</table>";
+
+		const firstDayOfMonth = moment(day).startOf("month");
+		const lastDayOfMonth = moment(day).endOf("month");
+		const numberOfDaysPastMonday = (firstDayOfMonth.day() - 1) % 7;
+		const numberOfDaysToFillFirstWeek = 7 - numberOfDaysPastMonday;
+
+		const firstWeekEmptyDays = R.map(
+			() => "<td/>",
+			R.range(0, numberOfDaysPastMonday)
+		);
+
+		const firstWeekFillingDays = R.map(
+			i => `<td class="dayNumber">${i}</td>`,
+			R.range(1, numberOfDaysToFillFirstWeek + 1)
+		);
+		const firstWeek = R.join("", [
+			...firstWeekEmptyDays,
+			...firstWeekFillingDays
+		]);
+		const weeksOtherThanFirst = R.pipe(
+			R.map(i => `<td class="dayNumber">${i}</td>`),
+			R.splitEvery(7),
+			R.map(s => R.join("", s))
+		)(R.range(numberOfDaysToFillFirstWeek + 1, lastDayOfMonth.date() + 1));
+
+		const content =
+			"<tr>" +
+			R.join("</tr><tr>", [firstWeek, ...weeksOtherThanFirst]) +
+			"</tr>";
+		contentsInHtml = contentsInHtml
+			.replace("%TITLE%", I18n.t("report.title"))
+			.replace("%DATE%", day.format(monthFormat))
+			.replace("%FIRST_DAY%", I18n.t("report.monday"))
+			.replace("%SECOND_DAY%", I18n.t("report.tuesday"))
+			.replace("%THIRD_DAY%", I18n.t("report.wednesday"))
+			.replace("%FOURTH_DAY%", I18n.t("report.thursday"))
+			.replace("%FIFTH_DAY%", I18n.t("report.friday"))
+			.replace("%SIXTH_DAY%", I18n.t("report.saturday"))
+			.replace("%SEVENTH_DAY%", I18n.t("report.sunday"))
+			.replace("%CONTENT%", content);
+
+		let options = {
+			html: contentsInHtml,
+			fileName: "Reporte"
+		};
+
+		let file = await RNHTMLtoPDF.convert(options);
+
+		await Share.open({
+			title: "This is my report ",
+			message: "Message:",
+			url: `file://${file.filePath}`,
+			subject: "Report"
+		});
+	};
 }
 
 export function fetchForbiddenFoodForDay(day) {
