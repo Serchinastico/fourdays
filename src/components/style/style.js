b902cc561a5d7e364dd0f941b3f32784d376fcdd
Serchinastico <54cymru@gmail.com>
2019-10-03 13:38:48 +0200

    Configure accept button enabled/disabled button

--- src/components/style/style.js
+++ src/components/style/style.js
@@ -16,6 +16,7 @@ export const color = {
 	brownGray: "#A1A1A1",
 	dorian: "#C0C0C0",
 	grass: "#95D163",
+	clearGrass: "#B6DF94",
 	darkMint: "#52BD76",
 	darkMint50: "#52BD7680",
 	seafoamGreen: "#87E0A5"
