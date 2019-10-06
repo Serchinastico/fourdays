cd50dc0aaae1e5291b9f3f245eccac44dd9adeba
Serchinastico <54cymru@gmail.com>
2019-10-06 23:48:58 +0200

    Migrate actions to the new system

--- src/addfood/types.ts
+++ src/addfood/types.ts
@@ -1,3 +1,5 @@
+import { action, payload, union } from "ts-action";
+
 export type Base64Image = string;
 
 export interface Base64FoodImage {
@@ -18,3 +20,29 @@ export interface CustomFood {
 	groupId: string;
 	image: FoodImage;
 }
+
+export const fetchCustomFoodListStart = action(
+	"Fetch custom food list [start]",
+);
+export const fetchCustomFoodListFinish = action(
+	"Fetch custom food list [finish]",
+	payload<[CustomFood]>(),
+);
+export const storeCustomFoodStart = action(
+	"Store custom food [start]",
+	payload<CustomFood>(),
+);
+export const storeCustomFoodFinish = action(
+	"Store custom food [finish]",
+	payload<CustomFood>(),
+);
+
+export const fetchCustomFoodActions = union(
+	fetchCustomFoodListStart,
+	fetchCustomFoodListFinish,
+);
+
+export const storeCustomFoodActions = union(
+	storeCustomFoodStart,
+	storeCustomFoodFinish,
+);
